9abf3389d22253ed70a7f82339999fdaae6226eb
gness1804 <graham.nessler@gmail.com>
2020-10-27 19:58:44 -0500

    Basic server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('testing...');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>My Site</h1>
+    </div>
+  `)
+});
+
+app.listen(3000, () => {
+  console.info('Listening on port 3000.')
+})
