e41a33c80ce01487f4e74cad7b0d16cd844fda2f
gness1804 <graham.nessler@gmail.com>
2020-11-21 17:03:53 -0600

    add post method to routes

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -7,5 +7,17 @@ export function get(path: string) {
     // desc: PropertyDescriptor,
   ) {
     Reflect.defineMetadata('path', path, classTarget, classKey);
+    Reflect.defineMetadata('method', 'get', classTarget, classKey);
+  };
+}
+
+export function post(path: string) {
+  return function (
+    classTarget: any,
+    classKey: string,
+    // desc: PropertyDescriptor,
+  ) {
+    Reflect.defineMetadata('path', path, classTarget, classKey);
+    Reflect.defineMetadata('method', 'post', classTarget, classKey);
   };
 }
